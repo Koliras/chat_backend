5f4bc01d03f508a440cbebfea10a2f83c5c1be94
Koliras <gyrmanchyk230506@gmail.com>
2024-09-04 05:50:59 +0000

    feat: add message in case user already exists

--- src/auth.rs
+++ src/auth.rs
@@ -1,6 +1,11 @@
 use std::sync::Arc;
 
-use axum::{extract::State, http::StatusCode, Json};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 use serde::{Deserialize, Serialize};
 
 use crate::AppState;
@@ -23,13 +28,13 @@ pub struct RegisterUser {
 pub async fn register(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<RegisterUser>,
-) -> StatusCode {
+) -> Response {
     let pass_encrypt_res = bcrypt::hash(payload.password.as_bytes(), 10);
     let password: String;
 
     match pass_encrypt_res {
         Ok(hash) => password = hash,
-        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
+        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
     }
 
     let result =
@@ -41,14 +46,18 @@ pub async fn register(
             .await;
 
     match result {
-        Ok(_) => StatusCode::CREATED,
+        Ok(_) => StatusCode::CREATED.into_response(),
         Err(e) => {
             if let Some(code) = e.as_database_error().unwrap().code() {
                 if code == "23505" {
-                    return StatusCode::CONFLICT;
+                    return (
+                        StatusCode::CONFLICT,
+                        "User with such nickname or email already exists",
+                    )
+                        .into_response();
                 }
             }
-            StatusCode::UNPROCESSABLE_ENTITY
+            StatusCode::UNPROCESSABLE_ENTITY.into_response()
         }
     }
 }
