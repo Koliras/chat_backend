df25e3ccb2f972ddae3b2b935bd7a4f977db4cba
Koliras <gyrmanchyk230506@gmail.com>
2024-09-05 19:16:07 +0000

    feat: add jwt token creation func

--- src/auth.rs
+++ src/auth.rs
@@ -7,6 +7,10 @@ use axum::{
     Json,
 };
 use bcrypt::BcryptResult;
+use jwt_simple::{
+    claims::Claims,
+    prelude::{HS256Key, MACLike},
+};
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 
@@ -113,4 +117,21 @@ pub async fn login(State(state): State<Arc<AppState>>, Json(payload): Json<Login
             .into_response(),
         Ok(_) => StatusCode::OK.into_response(),
     }
+}
+
+#[derive(Serialize, Deserialize)]
+struct JwtPayload {
+    username: String,
+}
+
+fn create_jwt_token(
+    username: String,
+    duration: jwt_simple::prelude::Duration,
+) -> Result<String, jwt_simple::Error> {
+    let key = HS256Key::from_bytes(
+        (std::env::var("JWT_SECRET").expect("JWT_SECRET have to be defined")).as_bytes(),
+    );
+    let claims = Claims::with_custom_claims(JwtPayload { username }, duration);
+
+    key.authenticate(claims)
 }
