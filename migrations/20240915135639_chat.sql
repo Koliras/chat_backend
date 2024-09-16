217a0085d68ca8165477c06ec93d1513f53da1fe
Koliras <gyrmanchyk230506@gmail.com>
2024-09-16 16:33:33 +0000

    add message to migration

--- migrations/20240915135639_chat.sql
+++ migrations/20240915135639_chat.sql
@@ -3,12 +3,28 @@ CREATE TABLE IF NOT EXISTS chat.chat (
 	name VARCHAR(50) NOT NULL,
 	admin_id INT,
 	FOREIGN KEY(admin_id) REFERENCES chat.user(id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS chat.user_chat (
 	user_id INT,
 	chat_id INT,
 	PRIMARY KEY(user_id, chat_id),
 	FOREIGN KEY(user_id) REFERENCES chat.user(id),
+	FOREIGN KEY(chat_id) REFERENCES chat.chat(id)
+);
+
+CREATE TABLE IF NOT EXISTS chat.message (
+	id SERIAL PRIMARY KEY,
+	content VARCHAR(400) NOT NULL,
+	user_id INT,
+	created_at TIMESTAMP DEFAULT(NOW()::timestamp),
+	FOREIGN KEY(user_id) REFERENCES chat.user(id)
+);
+
+CREATE TABLE IF NOT EXISTS chat.message_chat (
+	message_id INT,
+	chat_id INT,
+	PRIMARY KEY(message_id, chat_id),
+	FOREIGN KEY(message_id) REFERENCES chat.message(id),
 	FOREIGN KEY(chat_id) REFERENCES chat.chat(id)
 );
