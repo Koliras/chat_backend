1f0a9fa24595dcb09a8e135b59ad026cbfdfbc7b
Koliras <gyrmanchyk230506@gmail.com>
2024-09-20 19:49:48 +0000

    add route for deleting chat

--- src/chat.rs
+++ src/chat.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use axum::{middleware, routing::get, Router};
-use chat::{create_chat, get_chats};
+use chat::{create_chat, delete_chat, get_chats};
 
 use crate::middlewares::jwt_authorization;
 use crate::AppState;
@@ -10,7 +10,10 @@ pub mod chat;
 
 pub fn routes(shared_state: Arc<AppState>) -> Router<Arc<AppState>> {
     Router::new()
-        .route("/chat", get(get_chats).post(create_chat))
+        .route(
+            "/chat",
+            get(get_chats).post(create_chat).delete(delete_chat),
+        )
         .layer(middleware::from_fn_with_state(
             shared_state.clone(),
             jwt_authorization,
