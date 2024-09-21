34b6193bcd80ecc8effe523aecc06e0c9f0fd231
Koliras <gyrmanchyk230506@gmail.com>
2024-09-21 11:57:34 +0000

    add route to change chat name

--- src/chat.rs
+++ src/chat.rs
@@ -5,7 +5,7 @@ use axum::{
     routing::{delete, get},
     Router,
 };
-use chat::{create_chat, delete_chat, get_chats};
+use chat::{create_chat, delete_chat, get_chats, rename_chat};
 
 use crate::middlewares::jwt_authorization;
 use crate::AppState;
@@ -15,7 +15,7 @@ pub mod chat;
 pub fn routes(shared_state: Arc<AppState>) -> Router<Arc<AppState>> {
     Router::new()
         .route("/chat", get(get_chats).post(create_chat))
-        .route("/chat/:chat_id", delete(delete_chat))
+        .route("/chat/:chat_id", delete(delete_chat).patch(rename_chat))
         .layer(middleware::from_fn_with_state(
             shared_state.clone(),
             jwt_authorization,
